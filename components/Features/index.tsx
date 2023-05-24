9171cf0a8e1ea4a41da3eb9de20bcefa14e75af9
spark <parksh1000@outlook.com>
2023-05-24 16:47:40 +0900

    메인화면 구성중

---
+++ components/Features/index.tsx
@@ -0,0 +1,30 @@
+import SectionTitle from "../Common/SectionTitle";
+import SingleFeature from "./SingleFeature";
+import featuresData from "./featuresData";
+
+const Features = () => {
+  return (
+    <>
+      <section
+        id="features"
+        className="bg-primary/[.03] py-16 md:py-20 lg:py-28"
+      >
+        <div className="container">
+          <SectionTitle
+            title="중소기업 ESG 컨설팅서비스"
+            paragraph="정부와 공기업, 대기업이 ESG경영을 위한 준비가 마무리되고 있습니다.이제 중소기업도 ESG경영을 준비해야 할 때입니다. "
+            center
+          />
+
+          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
+            {featuresData.map((feature) => (
+              <SingleFeature key={feature.id} feature={feature} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Features;
